703157ce6ffa986823b660eb46445241a57f0b48
jhs512 <jangka512@gmail.com>
2024-03-21 16:09:03 +0900

    work

---
+++ src/main/java/com/ll/mm/MMApplication.java
@@ -0,0 +1,13 @@
+package com.ll.mm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MMApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ll/mm/answer/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.ll.mm.answer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
